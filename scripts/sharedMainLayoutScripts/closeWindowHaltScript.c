5c4046786151a99e098a02b576cf036ecce32db4
David Matson <githubdavid@gmail.com>
2014-02-09 10:31:39 -0800

    Create window closing script with halt command.
    I was selecting Halt command from the button options, but felt it is
    better practice to put all commands in scripts, not buttons for better
    transparency.

---
+++ scripts/sharedMainLayoutScripts/closeWindowHaltScript.c
@@ -0,0 +1,4 @@
+sharedMainLayoutScripts: closeWindowHaltScript
+Close Window [ Current Window ]
+Halt Script
+February 9, 平成26 10:24:51 Imagination Quality Management.fp7 - closeWindowHaltScript -1-
