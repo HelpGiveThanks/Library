4478bc737ca39f5f7d1db4488a5aa2c0d8a793d2
David Matson <githubdavid@gmail.com>
2014-04-26 14:21:34 -0700

    Add slash = / to warning message
    Add slash = / to warning message for file name character that prevent
    library from opening them.

--- scripts/reference-citation/URLcheck.c
+++ scripts/reference-citation/URLcheck.c
@@ -5,8 +5,9 @@ End If
 If [ Filter ( reference::ﬁleName ; "?" ) = "?" or
 Filter ( reference::ﬁleName ; "#" ) = "#" or
 Filter ( reference::ﬁleName ; ";" ) = ";" or
+Filter ( reference::ﬁleName ; "/" ) = "/" or
 Filter ( reference::ﬁleName ; ":" ) = ":" ]
-Show Custom Dialog [ Message: "These special characters ? # ; : prevent the system from opening ﬁles. Remove them from your ﬁlename and then test the 'ﬁle' button above to insure it opens your ﬁle."; Buttons: “OK” ]
+Show Custom Dialog [ Message: "These special characters ? # / ; : prevent the system from opening ﬁles. Remove them from your ﬁlename and then test the 'ﬁle' button above to insure it opens your ﬁle."; Buttons: “OK” ]
 Go to Field [ reference::ﬁleName ]
 End If
-January 7, 平成26 17:54:06 Imagination Quality Management.fp7 - URLcheck -1-
+April 26, 平成26 14:17:47 Imagination Quality Management.fp7 - URLcheck -1-
