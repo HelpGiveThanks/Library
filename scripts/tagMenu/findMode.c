5aaeeeee33dcddfefa16eadec4cb7d955e158b90
David Matson <githubdavid@gmail.com>
2014-07-30 11:18:38 -0700

    Change default find to tag(s). Also SEE NOTE
    New version of Chrome fixed an issue with reading ‘fi’ as one
    character, so everywhere fi is used it has been fixed to being to
    separate letters.

--- scripts/tagMenu/findMode.c
+++ scripts/tagMenu/findMode.c
@@ -1,29 +1,29 @@
-tagMenu: FindMode
+tagMenu: findMode
 #
-If [ $$ﬁndMode= "" ]
-#
-#
+If [ $$findMode= "" ]
 #
 #
-#If ﬁnd mode is active then return to browse mode.
+#
+#
+#If find mode is active then return to browse mode.
 If [ Get ( WindowMode ) = 1 ]
 #
-#If user presses enter ﬁnd button then perform the
-#steps below to complete the ﬁnd.
+#If user presses enter find button then perform the
+#steps below to complete the find.
 Perform Find [ ]
-Go to Layout [ $$ﬁndLayout ]
-Set Variable [ $$ﬁndLayout ]
+Go to Layout [ $$findLayout ]
+Set Variable [ $$findLayout ]
 #
 #Sort reference or tag records depending on which
 #records user is viewing.
 If [ Get (LayoutTableName) = "reference" ]
-Sort Records [ Speciﬁed Sort Order: tagKeywordPrimary::orderOrLock; ascending
+Sort Records [ Specified Sort Order: tagKeywordPrimary::orderOrLock; ascending
 tagKeywordPrimary::tag; ascending
 reference::referenceShort; ascending ]
 [ Restore; No dialog ]
 Halt Script
 Else
-Sort Records [ Speciﬁed Sort Order: ruleTagMenuGroups::order; based on value list: “order”
+Sort Records [ Specified Sort Order: ruleTagMenuGroups::order; based on value list: “order”
 ruleTagMenuGroups::name; ascending
 tagMenus::orderOrLock; based on value list: “order”
 tagMenus::tag; ascending ]
@@ -31,37 +31,37 @@ tagMenus::tag; ascending ]
 Halt Script
 End If
 #
-#If user presses enter ﬁnd button then perform the
-#steps below to complete the ﬁnd.
+#If user presses enter find button then perform the
+#steps below to complete the find.
 End If
 #
-#Ask user if they want to ﬁnd a tag or ﬁnd learn items
-#tagged with speciﬁc tags (except if they are in test).
+#Ask user if they want to find a tag or find learn items
+#tagged with specific tags (except if they are in test).
 If [ Get (LayoutTableName) ≠ "test" ]
 #
 #Show different message for learn and reference sections.
 If [ Left (Get (LayoutName) ; 9 ) ≠ "reference" ]
-Show Custom Dialog [ Message: "Find tagged learn records OR ﬁnd tag(s)? "; Buttons: “learn”, “tag(s)”, “cancel” ]
+Show Custom Dialog [ Message: "Find tagged learn records OR find tag(s)? "; Buttons: “tag(s)”, “learn”, “cancel” ]
 #
 Else If [ Left (Get (LayoutName) ; 9 ) = "reference" and $$referenceRecordOne ≠ 1 ]
 #
-#Eliminate ﬁnd reference option for citation menu.
+#Eliminate find reference option for citation menu.
 If [ $$citationMatch ≠ "cite" ]
-Show Custom Dialog [ Message: "Find tagged reference records OR ﬁnd tag(s)? "; Buttons: “reference”, “tag(s)”, “cancel” ]
+Show Custom Dialog [ Message: "Find tagged reference records OR find tag(s)? "; Buttons: “tag(s)”, “reference”, “cancel” ]
 Else
 Show Custom Dialog [ Message: "Find tag(s)? "; Buttons: “cancel”, “tag(s)” ]
 End If
 End If
 #
-#If user selects to ﬁnd tags enter ﬁnd mode ﬁrst.
+#If user selects to find tags enter find mode first.
 If [ Get ( LastMessageChoice ) = 2 and $$ClearMessageChoice ≠ 1 ]
 Enter Find Mode [ ]
 #
 #Determine if user is looking tag or reference records.
 If [ Get (LayoutTableName) = "reference" ]
 #
-#Get the layout name for the return trip after ﬁnd is done.
-Set Variable [ $$ﬁndLayout; Value:Get (LayoutName) ]
+#Get the layout name for the return trip after find is done.
+Set Variable [ $$findLayout; Value:Get (LayoutName) ]
 #
 #Show different layouts for learn and reference tag
 #menu items.
@@ -83,19 +83,19 @@ Else
 Set Field [ reference::kcsection; TEMP::ksection ]
 End If
 #
-#Select the ﬁeld where the user needs to enter the
-#ﬁnd information.
+#Select the field where the user needs to enter the
+#find information.
 Go to Field [ reference::referenceForReferenceWindow ]
 #
-#Pause and wait until user is ready to perform ﬁnd.
-Pause/Resume Script [ Indeﬁnitely ]
+#Pause and wait until user is ready to perform find.
+Pause/Resume Script [ Indefinitely ]
 #
 #If user presses enter key then perform the steps below
-#to complete the ﬁnd.
+#to complete the find.
 Perform Find [ ]
-Go to Layout [ $$ﬁndLayout ]
-Set Variable [ $$ﬁndLayout ]
-Sort Records [ Speciﬁed Sort Order: tagKeywordPrimary::orderOrLock; ascending
+Go to Layout [ $$findLayout ]
+Set Variable [ $$findLayout ]
+Sort Records [ Specified Sort Order: tagKeywordPrimary::orderOrLock; ascending
 tagKeywordPrimary::tag; ascending
 reference::referenceShort; ascending ]
 [ Restore; No dialog ]
@@ -107,11 +107,11 @@ Else
 #learn mode if in learn mode.
 Set Field [ tagMenus::match; $$citationMatch ]
 Set Field [ ruleTagMenuGroups::ksection; "==" & TEMP::ksection ]
-Set Variable [ $$ﬁndLayout; Value:Get (LayoutName) ]
+Set Variable [ $$findLayout; Value:Get (LayoutName) ]
 If [ Left (Get (LayoutName) ; 9 ) ≠ "reference" ]
 If [ Left (Get (LayoutName) ; 5 ) = "learn" ]
 Go to Layout [ “learnMenuFINDrefLearn” (testlearn) ]
-#Clear these variables so ﬁnd will work.
+#Clear these variables so find will work.
 Set Field [ tagMenus::match; "" ]
 Set Field [ ruleTagMenuGroups::ksection; "" ]
 Else
@@ -119,29 +119,29 @@ Go to Layout [ “ltagFINDNK” (tagMenus) ]
 End If
 #
 Else
-January 7, 平成26 16:39:01 Imagination Quality Management.fp7 - FindMode -1-tagMenu: FindMode
+July 30, 平成26 11:14:36 Imagination Quality Management.fp7 - findMode -1-tagMenu: findMode
 #Show all reference items for a section if in
 #reference mode.
 Go to Layout [ “referenceFINDNK” (tagMenus) ]
 End If
 #
-#Select the ﬁeld where the user needs to enter the
-#ﬁnd information.
+#Select the field where the user needs to enter the
+#find information.
 If [ Left (Get (LayoutName) ; 5 ) = "learn" ]
 Go to Field [ testlearn::Caption ]
 Else
 Go to Field [ tagMenus::tag ]
 End If
 #
-#Pause and wait until user is ready to perform ﬁnd.
-Pause/Resume Script [ Indeﬁnitely ]
+#Pause and wait until user is ready to perform find.
+Pause/Resume Script [ Indefinitely ]
 #
 #If user presses enter key then perform the steps below
-#to complete the ﬁnd.
+#to complete the find.
 Perform Find [ ]
-Go to Layout [ $$ﬁndLayout ]
-Set Variable [ $$ﬁndLayout ]
-Sort Records [ Speciﬁed Sort Order: ruleTagMenuGroups::order; based on value list: “order”
+Go to Layout [ $$findLayout ]
+Set Variable [ $$findLayout ]
+Sort Records [ Specified Sort Order: ruleTagMenuGroups::order; based on value list: “order”
 ruleTagMenuGroups::name; ascending
 tagMenus::orderOrLock; based on value list: “order”
 tagMenus::tag; ascending ]
@@ -158,18 +158,18 @@ End If
 #
 #
 #
-#So if user wants to ﬁnd learn records, then go into
-#menu for ﬁnding learn records by green button tags.
+#So if user wants to find learn records, then go into
+#menu for finding learn records by green button tags.
 #
 #Find must be done for tags, so if user is showing no
-#tags when ﬁnd is activitated then take user to keywords
+#tags when find is activitated then take user to keywords
 #tag menu.
 If [ Get (LayoutName) = "ReferenceMenuNoTags" ]
 Go to Layout [ “ReferenceMenu2keywordOrNode1” (tagMenus) ]
 End If
 #
-Set Variable [ $$ﬁndMode; Value:1 ]
-Set Variable [ $$ﬁndLayout; Value:Get (LayoutName) ]
+Set Variable [ $$findMode; Value:1 ]
+Set Variable [ $$findLayout; Value:Get (LayoutName) ]
 If [ Right ( Get (LayoutName) ; 7 ) = "refcite" and Left ( Get (LayoutName) ; 1 ) = "l" ]
 Go to Layout [ “learnMenu4RefCiteFindTL” (testlearn) ]
 Else If [ Right ( Get (LayoutName) ; 5 ) = "menu1" and Left ( Get (LayoutName) ; 1 ) = "l" ]
@@ -196,36 +196,36 @@ Else If [ Right ( Get (LayoutName) ; 1 ) = "3" and Left ( Get (LayoutName) ; 1 )
 Go to Layout [ “ReferenceMenu2keywordOrNodeFind” (tagMenus) ]
 End If
 #
-#Clear the ﬁrst record question variable so user will
-#not be asked if they wish to ﬁnd or tag reference records.
+#Clear the first record question variable so user will
+#not be asked if they wish to find or tag reference records.
 Set Variable [ $$referenceRecordOne ]
 Set Variable [ $$ClearMessageChoice ]
 #
 #As going to the other window will be involved
 #stop the record load script on that window until
-#this script is ﬁnished to speed things up and
-#stop ﬂashing effect.
+#this script is finished to speed things up and
+#stop flashing effect.
 // Set Variable [ $$stoploadCitation; Value:1 ]
-Select Window [ Name: "References"; Current ﬁle ]
+Select Window [ Name: "References"; Current file ]
 If [ Get (LayoutName) = "ReferenceEDIT" ]
 Go to Layout [ “Reference” (reference) ]
 End If
-Select Window [ Name: "Learn"; Current ﬁle ]
+Select Window [ Name: "Learn"; Current file ]
 If [ Get (LayoutName) = "Learn4Edit" ]
 Go to Layout [ “learn4” (testlearn) ]
 End If
 Refresh Window
-Select Window [ Name: "Tag Menus"; Current ﬁle ]
+Select Window [ Name: "Tag Menus"; Current file ]
 Exit Script [ ]
-Else If [ $$ﬁndMode= 1 ]
-Set Variable [ $$ﬁndMode ]
-Set Variable [ $$ﬁrstFind ]
-Set Variable [ $$ﬁrstFindOther ]
+Else If [ $$findMode= 1 ]
+Set Variable [ $$findMode ]
+Set Variable [ $$firstFind ]
+Set Variable [ $$firstFindOther ]
 Set Variable [ $$found ]
 Set Variable [ $$foundOther ]
-Go to Layout [ $$ﬁndLayout ]
+Go to Layout [ $$findLayout ]
 Set Variable [ $$stoploadCitation ]
 Exit Script [ ]
 End If
 #
-January 7, 平成26 16:39:01 Imagination Quality Management.fp7 - FindMode -2-
+July 30, 平成26 11:14:36 Imagination Quality Management.fp7 - findMode -2-
