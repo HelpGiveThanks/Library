3df7e69b8f231c7514732bb8d55eb6ead1de0b3b
David Matson <githubdavid@gmail.com>
2015-07-11 11:11:02 -0700

    Change language to improve quality and update to new vocabulary.
    Instead of ‘discovery’ now using ‘test result’ and instead of ‘focus’
    now use ‘test section.’

--- scripts/testScreens/testReport/deleteInspectionLocation.c
+++ scripts/testScreens/testReport/deleteInspectionLocation.c
@@ -1,16 +1,16 @@
-testScreens: testReport: DeleteInspectionLocation
+testScreens: testReport: deleteInspectionLocation
 If [ tagTestSubjectLocation::_LtestSubjectLocation = "" ]
 Halt Script
 End If
 If [ tagTestSubjectLocation::inUse = "t" ]
-Show Custom Dialog [ Title: "!"; Message: "Deleting this set of test items is allowed after you delete all discoveries made during testing."; Buttons: “OK” ]
+Show Custom Dialog [ Message: "Delete all test results made in this test section before deleting it. To do this, click its green test button. Click on each test item. Delete the results you find."; Buttons: “OK” ]
 Exit Script [ ]
 End If
 Set Variable [ $delete; Value:tagTestSubjectLocation::_LtestSubjectLocation ]
 Set Variable [ $location; Value:tagTestSubjectLocation::kfocus ]
 Go to Field [ ]
 Refresh Window
-Show Custom Dialog [ Title: "!"; Message: "Delete " & tagTestSubject::tag & "'s " & "test number " & tagTestSubjectLocation::reportNumber & " of " & tagTestSubjectLocation::focusName & "?"; Buttons: “Cancel”, “Delete” ]
+Show Custom Dialog [ Message: "Delete test section " & tagTestSubjectLocation::focusName & " for " & tagTestSubject::tag & "'s test #" & tagTestSubjectLocation::reportNumber & "?"; Buttons: “Cancel”, “Delete” ]
 If [ Get ( LastMessageChoice ) = 2 ]
 Delete Record/Request
 [ No dialog ]
@@ -18,4 +18,4 @@ End If
 Go to Layout [ original layout ]
 Set Variable [ $delete ]
 Refresh Window
-January 7, 平成26 14:16:54 Imagination Quality Management.fp7 - DeleteInspectionLocation -1-
+July 11, 平成27 11:08:18 Library.fp7 - deleteInspectionLocation -1-
