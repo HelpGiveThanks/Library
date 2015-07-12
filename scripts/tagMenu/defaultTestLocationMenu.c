7cdddbf4f508b9c4e7cd93292e243103ba548258
David Matson <githubdavid@gmail.com>
2015-07-12 14:09:30 -0700

    Update Test menu script to insert Test Subject's name.

--- scripts/tagMenu/defaultTestLocationMenu.c
+++ scripts/tagMenu/defaultTestLocationMenu.c
@@ -1,5 +1,15 @@
 tagMenu: defaultTestLocationMenu
 #
+#WHEN TIME PERMITS change 'location' to
+#'section' for test section. When the database
+#was first being created it was for testing
+#locations in a building, but as it became a general
+#testing app, 'location' gave way to test 'section.'
+#So change has been made on the layouts, but not
+#yet in the script or field name which requires
+#time for testing, going thru the DDR to find all occurances, etc.
+#
+#
 If [ tempSetup::kdefaultNodePrimary = "" ]
 Show Custom Dialog [ Message: "Select yourself (the node responsible for creating new records) by clicking the node button."; Buttons: “OK” ]
 Halt Script
@@ -8,11 +18,12 @@ Show Custom Dialog [ Message: "Select the default copyright for any new records 
 Halt Script
 End If
 #
-#A section must be selected before locations
-#can be assigned.
+#A section template must be selected before new
+#sections can be created, which are based on/duplicated
+#from a section template.
 If [ TEMP::ksection = "" ]
 Perform Script [ “defaultSectionMenu” ]
-Show Custom Dialog [ Message: "You must select a section prior to clicking the 'test' button. All tests belong to specific sections. "; Buttons: “OK” ]
+Show Custom Dialog [ Message: "You must select a section template prior to clicking the 'test' button. All test sections are created from a section template. "; Buttons: “OK” ]
 Exit Script [ ]
 End If
 #
@@ -30,19 +41,20 @@ Perform Script [ “addBackSectionCreatorNode” ]
 #Set citationMatch to color menu button with inUse color.
 Set Variable [ $$citationMatch; Value:"location" ]
 #
-#Locations are shown are the general locations
-#for a test. This menu will be used to create
-#specific locations for a specific test subject.
+#Sections shown are the template sections
+#for a test. This tag window will be used to create
+#specific sections from a template for test subjects.
 #So in the Setup window the system highlights
-#the test subject for whom the locations are going
-#to be created.
+#the test subject for whom the sections are going
+#to be created, and who are going to be tested.
 Select Window [ Name: "Setup"; Current file ]
 Set Variable [ $$citationitem ]
 Set Variable [ $$testSubject; Value:tempSetup::kdefaultNodeTestSubject ]
+Set Field [ TEMP::DEFAULTNodeTestSubjectName; tagDefaultTestSubject::tag ]
 Refresh Window
 Select Window [ Name: "Tag Menus"; Current file ]
 #
-#Find all test location records.
+#Find all created test section records.
 Go to Layout [ “defaultTest” (tagTestSubjectLocation) ]
 Allow User Abort [ Off ]
 Set Error Capture [ On ]
@@ -68,7 +80,7 @@ Refresh Window
 #goto Tag Menus window
 Select Window [ Name: "Tag Menus"; Current file ]
 #
-#Clear current temp field of location names in
+#Clear current temp field of section names in
 #case user has selected a section different from
 #the one previous, and thus with name that might
 #not match those in these temp fields.
@@ -86,7 +98,7 @@ Set Field [ TEMP::orderMod2; "" ]
 Set Field [ TEMP::reportNumber; 1 ]
 #
 #Set temp keys to unlock pulldown lists for
-#section locations.
+#section templates.
 Go to Layout [ “tableGroupTag” (groupTest) ]
 Enter Find Mode [ ]
 Set Field [ groupTest::ksection; TEMP::ksection ]
@@ -95,7 +107,8 @@ Perform Find [ ]
 Set Field [ TEMP::locationSection; groupTest::_Lgroup ]
 #
 #Set temp keys to unlock pulldown lists for
-#section attributes.
+#section attributes. NOTE: I DO NOT THINK these
+#attirbutes are in use any longer. Testing required.
 Enter Find Mode [ ]
 Set Field [ groupTest::ksection; TEMP::ksection ]
 Set Field [ groupTest::match; "attribute" ]
@@ -104,4 +117,4 @@ Set Field [ TEMP::pulldownAttribute; groupTest::_Lgroup ]
 #
 #Return to tag menu layout.
 Go to Layout [ “defaultTest” (tagTestSubjectLocation) ]
-January 28, 平成26 15:52:43 Empty Library copy.fp7 - defaultTestLocationMenu -1-
+July 12, 平成27 14:06:10 Library.fp7 - defaultTestLocationMenu -1-
