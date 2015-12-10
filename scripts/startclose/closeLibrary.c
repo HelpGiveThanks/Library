a19b3eaf0e660ee84023922bfa30513329dccc8f
David Matson <githubdavid@gmail.com>
2015-12-09 18:49:22 -0800

    Remove unneeded script steps.

--- scripts/startclose/closeLibrary.c
+++ scripts/startclose/closeLibrary.c
@@ -1,18 +1,4 @@
 startclose: closeLibrary
-// Go to Layout [ “startMemorySwitch” (MemorySwitch) ]
-// Show All Records
-// Go to Record/Request/Page
-[ First ]
-// Loop
-// Exit Loop If [ tempSetup::filePath ≠ MemorySwitch::path ]
-// Go to Record/Request/Page
-[ Next; Exit after last ]
-// End Loop
-// If [ tempSetup::filePath ≠ MemorySwitch::path ]
-// New Record/Request
-// Set Field [ MemorySwitch::path; TEMP::filePath ]
-// Set Field [ MemorySwitch::name; TEMP::fileName ]
-// End If
 Close Window [ Current Window ]
 Close Window [ Name: "Tag Menus"; Current file ]
-January 7, 平成26 12:01:44 Imagination Quality Management.fp7 - closeLibrary -1-
+December 9, 2015 12:01:44 Imagination Quality Management.fp7 - closeLibrary -1-
