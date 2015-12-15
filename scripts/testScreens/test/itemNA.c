8ddaecc47571f593af2207dd26c75a285d3fe91e
David Matson <githubdavid@gmail.com>
2015-12-15 15:59:58 -0800

    Removed star symbol as possible test label

--- scripts/testScreens/test/itemNA.c
+++ scripts/testScreens/test/itemNA.c
@@ -1,4 +1,13 @@
-testScreens: test: ItemNA
+testScreens: test: itemNA
+#
+#
+#This script creates test result that is not shown
+#as a test result.
+#Instead, this record is used to show an N/A or OK
+#next to a test. If the user decides to not show
+#the OK, then this script deletes this test result record.
+#
+#
 #basic administration tasks
 Set Error Capture [ On ]
 Allow User Abort [ Off ]
@@ -50,21 +59,28 @@ Go to Field [ ]
 Exit Script [ ]
 Else If [ tlResults::InspectionItemCountLocation = "N/A" or
 tlResults::InspectionItemCountLocation = "OK" ]
-If [ TLResultsOnReport::InspectionItemCount = "" ]
+#
+#
+#Disable star symbol.
+// If [ TLResultsOnReport::InspectionItemCount = "" ]
+If [ TLResultsOnReport::InspectionItemCount = "" or TLResultsOnReport::InspectionItemCount ≠ "" ]
 Set Field [ TEMP::testName ]
-Show Custom Dialog [ Title: "FYI"; Message: "Use the star symbol to referr to other assessment findings. You currently do not have any for this item."; Buttons: “OK” ]
+// Show Custom Dialog [ Title: "FYI"; Message: "Use the star symbol to referr to other assessment findings. You
+currently do not have any for this item."; Buttons: “OK” ]
 Set Field [ tlResults::InspectionItemCountLocation; "" ]
 Go to Field [ ]
 Freeze Window
 New Window [ ]
 #note item's ID number and name
 Set Variable [ $$item; Value:InspectItems::_Ltest ]
+#
 Go to Layout [ “discoveries” (testlearn) ]
 Enter Find Mode [ ]
 Set Field [ testlearn::ktestSubject; TEMP::kdefaultNodeTestSubject ]
 Set Field [ testlearn::kaudienceLocation; $$location ]
 Set Field [ testlearn::ktest; $$item ]
 Perform Find [ ]
+#
 Delete Record/Request
 [ No dialog ]
 #Unlock Item Location
@@ -72,6 +88,7 @@ Enter Find Mode [ ]
 Set Field [ testlearn::ktestSubject; TEMP::kdefaultNodeTestSubject ]
 Set Field [ testlearn::kaudienceLocation; $$location ]
 Perform Find [ ]
+#
 If [ Get ( LastError ) = 401 ]
 Go to Layout [ “tableTestSubjectFocus” (tagTestSubjectLocation) ]
 Enter Find Mode [ ]
@@ -83,19 +100,20 @@ Enter Find Mode [ ]
 Set Field [ tagTestSubjectLocation::knode; TEMP::kdefaultNodeTestSubject ]
 Perform Find [ ]
 Go to Layout [ “step3_InspectionItems” (InspectItems) ]
+#
 Set Field [ InspectItems::gprogressGlobal; "status: pending" ]
 End If
 Close Window [ Current Window ]
 Exit Script [ ]
 End If
-Set Field [ tlResults::InspectionItemCountLocation; "★" ]
+Set Field [ tlResults::InspectionItemCountLocation; "̣" ]
 Go to Layout [ “step3_InspectionItems” (InspectItems) ]
 Set Field [ InspectItems::gprogressGlobal; "status: in progress" ]
 Go to Field [ ]
 Exit Script [ ]
 Else If [ tlResults::InspectionItemCountLocation ≠ "N/A" and
 tlResults::InspectionItemCountLocation ≠ "OK" and
-tlResults::InspectionItemCountLocation ≠ "★" ]
+tlResults::InspectionItemCountLocation ≠ "̣" ]
 Set Field [ TEMP::testName ]
 Go to Layout [ “step3_InspectionItems” (InspectItems) ]
 Set Field [ InspectItems::gprogressGlobal; "status: in progress" ]
@@ -117,7 +135,6 @@ Delete Record/Request
 [ No dialog ]
 #Unlock Item Location
 Enter Find Mode [ ]
-January 7, 平成26 12:42:43 Imagination Quality Management.fp7 - ItemNA -1-testScreens: test: ItemNA
 Set Field [ testlearn::ktestSubject; TEMP::kdefaultNodeTestSubject ]
 Set Field [ testlearn::kaudienceLocation; $$location ]
 Perform Find [ ]
@@ -135,4 +152,4 @@ Go to Layout [ “step3_InspectionItems” (InspectItems) ]
 Set Field [ InspectItems::gprogressGlobal; "status: pending" ]
 End If
 Close Window [ Current Window ]
-January 7, 平成26 12:42:43 Imagination Quality Management.fp7 - ItemNA -2-
+December 15, ଘ౮27 15:57:59 Library.fp7 - itemNA -1-
