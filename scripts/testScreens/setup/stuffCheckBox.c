2ff0d3cfdd0920c179616e8d0738c376f787d22a
David Matson <githubdavid@gmail.com>
2015-12-27 19:07:21 -0800

    Make library type checkbox part of the library record.

--- scripts/testScreens/setup/stuffCheckBox.c
+++ scripts/testScreens/setup/stuffCheckBox.c
@@ -1,14 +1,26 @@
-testScreens: setup: stuffCheckBox
+changeLibraryOrLibraryName: stuffCheckBox
+#
+#Exit the checkbox.
 Go to Field [ ]
+#
+#Set default layouts for a reference library.
 If [ tempSetup::InventoryLibaryYN = "" ]
 Set Field [ tempSetup::layoutLtagK; "" ]
 Set Field [ tempSetup::layoutRtagK; "" ]
 Set Field [ tempSetup::layoutLtagN; "" ]
 Set Field [ tempSetup::layoutRtagN; "" ]
+#
+#Designate library as a reference library.
+Set Field [ sectionAttributionInfo::order; "" ]
 Else
+#
+#Set default layouts for a stuff/inventory library.
 Set Field [ tempSetup::layoutLtagK; "moreltagNKs2" ]
 Set Field [ tempSetup::layoutRtagK; "moreReferenceMenu2SkeywordOrNode1" ]
 Set Field [ tempSetup::layoutLtagN; "moreltagNKs2" ]
 Set Field [ tempSetup::layoutRtagN; "moreReferenceMenu2SkeywordOrNode1" ]
+#
+#Designate library as a stuff/inventory library.
+Set Field [ sectionAttributionInfo::order; 1 ]
 End If
-May 4, 平成27 21:28:07 Library.fp7 - stuffCheckBox -1-
+December 27, ଘ౮27 19:05:50 Library.fp7 - stuffCheckBox -1-
