50eb15b91169be7e77fe9d8af670893e84a1de9e
David Matson <githubdavid@gmail.com>
2015-12-28 16:05:59 -0800

    Fixes bug #6
    Fixes $6 — Test result records shouldn’t be seen in the Learn module.

--- scripts/pictures/addLinksPicturesToTagsMode.c
+++ scripts/pictures/addLinksPicturesToTagsMode.c
@@ -6,7 +6,8 @@ Set Error Capture [ On ]
 #If node is currenlty locked then stop script, inform user.
 If [ nodeLockTagMenus::orderOrLock ≠ "" ]
 Go to Field [ ]
-Show Custom Dialog [ Message: "This record is locked. Go the node that created it -- " & nodeLockTagMenus::tag & " -- and enter the password to unlock it and then you will be able to add pictures and links to it."; Buttons: “OK” ]
+Show Custom Dialog [ Message: "This record is locked. Go the node that created it -- " & nodeLockTagMenus::tag & " -- and
+enter the password to unlock it and then you will be able to add pictures and links to it."; Buttons: “OK” ]
 Exit Script [ ]
 End If
 If [ nodeLockTagMenus::orderOrLock ≠ "" and tagMenus::match = "sample" or
@@ -15,7 +16,8 @@ nodeLockTagMenus::orderOrLock ≠ "" and tagMenus::match = "health" or
 nodeLockTagMenus::orderOrLock ≠ "" and tagMenus::match = "health" or
 nodeLockTagMenus::orderOrLock ≠ "" and tagMenus::match = "health" or
 nodeLockTagMenus::orderOrLock ≠ "" and tagMenus::match = "test" ]
-Show Custom Dialog [ Message: "This record is currently locked. Select the node that created it and enter the password to unlock it, then you will able to start the delete process."; Buttons: “OK” ]
+Show Custom Dialog [ Message: "This record is currently locked. Select the node that created it and enter the password to
+unlock it, then you will able to start the delete process."; Buttons: “OK” ]
 Exit Script [ ]
 Else If [ tagMenus::orderOrLock ≠ "" and tagMenus::match = "health" ]
 Show Custom Dialog [ Message: "This record is locked by the system."; Buttons: “OK” ]
@@ -118,36 +120,43 @@ Go to Layout [ “ReferenceAddToTag” (reference) ]
 // #more sense to have the user check a keyword
 // #checkbox item than a key item as key is short
 // #for keyword in this case.
-May 10, 平成27 11:19:45 Library.fp7 - addLinksPicturesToTagsMode -1-
-pictures: addLinksPicturesToTagsMode
 // #( You can see these checkboxes at the bottom
 // # of every Reference record )
 // If [ $$citationMatch = "key" ]
 // Set Field [ reference::filterFind; "*" & "keyword" ]
 // #
 // #If the citationMatch equals any other tag type ...
-// Else If [ $$citationMatch
-≠ "key" ]
+// Else If [ $$citationMatch ≠ "key" ]
 // Set Field [ reference::filterFind; "*" & Quote ( $$citationMatch ) ]
 // End If
 // #
-// Perform Find [ ] #
+// Perform Find [ ]
+#
 #Clear the stop load record variable and load
 #the current record's info.
 Set Variable [ $$stoploadCitation ]
-Perform Script [ “loadCitation” ] #
+Perform Script [ “loadCitation” ]
+#
 #Go to tag menu window.
 Select Window [ Name: "Tag Menus"; Current file ]
-Exit Script [ ] #
-#END start add mode ####
-#BEGIN stop add mode #
+Exit Script [ ]
+#
+#END start add mode
+#
+#
+#
+#
+#BEGIN stop add mode
+#
 #If in add mode then clear the add variable to conditionally
 #format the 'add' button to indicate that add mode
 #has been turned off.
 Else If [ $$add = 1 ]
-Set Variable [ $$add ] #
+Set Variable [ $$add ]
+#
 #If the user was in test mode prior to add mode ...
-If [ $$citationMatch = "testItem" ] #
+If [ $$citationMatch = "testItem" ]
+#
 #This may be lazy, but instead of doing an if
 #test to figure out what window a user is in,
 #it seems that telling the system to select both
@@ -155,7 +164,8 @@ If [ $$citationMatch = "testItem" ] #
 #in works just fine. The system always
 #selects the one the user is in.
 Select Window [ Name: "References"; Current file ]
-Select Window [ Name: "Learn"; Current file ] #
+Select Window [ Name: "Learn"; Current file ]
+#
 #Now loop to the record the user was on,
 #and turn off the load record script to speed things
 #up. ( This record set does not need to be found
@@ -167,25 +177,30 @@ Loop
 Exit Loop If [ test::_Ltest = $$returnRecord ]
 Go to Record/Request/Page
 [ Next; Exit after last ]
-End Loop #
+End Loop
+#
 #Set the window title to Setup and turn back on
 #the load record script, but do not load the record
 #as this record's variables are already loaded.
 Set Window Title [ Current Window; New Title: "Setup" ]
-Set Variable [ $$ID; Value:test::_Ltest ] #
+Set Variable [ $$ID; Value:test::_Ltest ]
+#
 #Go to the tag menu and perform the TestItem
 #menu script to reset the conditional formatting.
 Select Window [ Name: "Tag Menus"; Current file ]
 Go to Layout [ “setupTestItem” (tagMenus) ]
-Perform Script [ “menuTestItem” ] #
+Perform Script [ “menuTestItem” ]
+#
 #Clear all add mode variables and exit script.
 Set Variable [ $$returnWindow ]
 Set Variable [ $$returnLayout ]
 Set Variable [ $$returnRecord ]
 Set Variable [ $$tagLayout ]
-Exit Script [ ] #
+Exit Script [ ]
+#
 #If the user was in default mode prior to add mode ...
-Else If [ $$returnWindow = "defaultSetup" ] #
+Else If [ $$returnWindow = "defaultSetup" ]
+#
 #This may be lazy, but instead of doing an if
 #test to figure out what window a user is in,
 #it seems that telling the system to select both
@@ -193,32 +208,36 @@ Else If [ $$returnWindow = "defaultSetup" ] #
 #in works just fine. The system always
 #selects the one the user is in.
 Select Window [ Name: "References"; Current file ]
-Select Window [ Name: "Learn"; Current file ] #
+Select Window [ Name: "Learn"; Current file ]
+#
 #There is always only one default record, so
 #finding and looping to record is not necessary.
 Go to Layout [ “defaultSetup” (tempSetup) ]
-Set Window Title [ Current Window; New Title: "Setup" ] #
+Set Window Title [ Current Window; New Title: "Setup" ]
+#
 #Go to the tag menu and perform the Node
 #menu script to reset the conditional formatting.
 Select Window [ Name: "Tag Menus"; Current file ]
 Go to Layout [ “defaultNode1” (tagMenus) ]
-Perform Script [ “defaultNodeMenu” ] #
+Perform Script [ “defaultNodeMenu” ]
+#
 #Clear all add mode variables and exit script.
 Set Variable [ $$returnWindow ]
 Set Variable [ $$returnLayout ]
 Set Variable [ $$returnRecord ]
 Set Variable [ $$tagLayout ]
 Exit Script [ ]
-End If #
+End If
+#
 #If going back to a Learn or Reference layout,
 #then first turn off the record load variable.
 #I decided to do it here rather than include it
 #each time before it is needed.
-Set Variable [ $$stoploadCitation; Value:1 ] #
+Set Variable [ $$stoploadCitation; Value:1 ]
+#
 #If the user was in learn mode prior to add mode ...
 If [ $$returnWindow = "Learn" ]
-May 10, 平成27 11:19:45 Library.fp7 - addLinksPicturesToTagsMode -2-
-pictures: addLinksPicturesToTagsMode #
+#
 #Go to tag layout user was on before entering
 #add mode. ( This may not be useful as user
 # could also predict that tag window will stay
@@ -226,11 +245,13 @@ pictures: addLinksPicturesToTagsMode #
 # not remember what tag they where on when
 # they started add mode. Have to see if this
 # is useful or not. )
-Select Window [ Name: "Tag Menus"; Current file ] #
+Select Window [ Name: "Tag Menus"; Current file ]
+#
 If [ $$citationMatch = "node" or
 $$citationMatch = "key" or
 $$citationMatch = "testItem" ]
-Go to Layout [ $$tagLayout ] #
+Go to Layout [ $$tagLayout ]
+#
 Else If [ $$citationMatch = "medium" or
 $$citationMatch = "health" or
 $$citationMatch = "organ" or
@@ -238,7 +259,8 @@ $$citationMatch = "copyist" or
 //$$citationMatch = "sample" or
 $$citationMatch = "path" ]
 Go to Layout [ $$tagLayout ]
-End If #
+End If
+#
 #This may be lazy, but instead of doing an if
 #test to figure out what window a user is in,
 #it seems that telling the system to select both
@@ -248,12 +270,14 @@ End If #
 Select Window [ Name: "References"; Current file ]
 Select Window [ Name: "Learn"; Current file ]
 Set Window Title [ Current Window; New Title: "Learn" ]
-Go to Layout [ $$returnLayout ] #
+Go to Layout [ $$returnLayout ]
+#
 #Find main learn records.
 Enter Find Mode [ ]
 Set Field [ testlearn::kcsection; TEMP::ksection ]
-// Set Field [ testlearn::filterFind; "main" & ¶ ]
-Perform Find [ ] #
+Set Field [ testlearn::filterFind; "main" & ¶ ]
+Perform Find [ ]
+#
 #Go to record user was on before add mode.
 Sort Records [ Specified Sort Order: testlearn::date; descending
 testlearn::timestamp; descending ]
@@ -266,16 +290,19 @@ Loop
 Exit Loop If [ testlearn::_Ltestlearn = $$returnRecord ]
 Go to Record/Request/Page
 [ Next; Exit after last ]
-End Loop #
+End Loop
+#
 #Clear all add mode variables and exit script.
 Set Variable [ $$returnWindow ]
 Set Variable [ $$returnLayout ]
 Set Variable [ $$returnRecord ]
 Set Variable [ $$tagLayout ]
 Set Variable [ $$stoploadCitation ]
-Perform Script [ “loadCitation” ] #
+Perform Script [ “loadCitation” ]
+#
 #If the user was in reference mode prior to add mode ...
-Else If [ $$returnWIndow = "References" ] #
+Else If [ $$returnWIndow = "References" ]
+#
 #Go to tag layout user was on before entering
 #add mode. ( This may not be useful as user
 # could also predict that tag window will stay
@@ -283,21 +310,26 @@ Else If [ $$returnWIndow = "References" ] #
 # not remember what tag they where on when
 # they started add mode. Have to see if this
 # is useful or not. )
-Select Window [ Name: "Tag Menus"; Current file ] #
+Select Window [ Name: "Tag Menus"; Current file ]
+#
 #Get the record number.
-Set Variable [ $recordNumber; Value:Get (RecordNumber) ] #
+Set Variable [ $recordNumber; Value:Get (RecordNumber) ]
+#
 If [ $$citationMatch = "node" or
 $$citationMatch = "key" or
 $$citationMatch = "testItem" ]
-Go to Layout [ Middle ( TEMP::layoutRtagK ; 5 ; 42 ) ] #
+Go to Layout [ Middle ( TEMP::layoutRtagK ; 5 ; 42 ) ]
+#
 Else If [ $$citationMatch = "medium" or
 $$citationMatch = "organ" or
 $$citationMatch = "copyist" or
 $$citationMatch = "path" ]
-Go to Layout [ “ReferenceMenu1” (tagMenus) ] #
+Go to Layout [ “ReferenceMenu1” (tagMenus) ]
+#
 Else If [ $$citationMatch = "health" ]
 Go to Layout [ “ReferenceMenu1” (tagMenus) ]
-End If #
+End If
+#
 #This may be lazy, but instead of doing an if
 #test to figure out what window a user is in,
 #it seems that telling the system to select both
@@ -307,19 +339,20 @@ End If #
 Select Window [ Name: "References"; Current file ]
 Select Window [ Name: "Learn"; Current file ]
 Set Window Title [ Current Window; New Title: "References" ]
-If [ TEMP::InventoryLibaryYN
-≠ "" ]
+If [ TEMP::InventoryLibaryYN ≠ "" ]
 Go to Layout [ “ReferenceStuff” (reference) ]
 Else
 Go to Layout [ “Reference” (reference) ]
-End If #
+End If
+#
 #Find main reference records.
 Set Error Capture [ On ]
 Allow User Abort [ Off ]
 Enter Find Mode [ ]
 Set Field [ reference::kcsection; TEMP::ksection ]
 // Set Field [ reference::filterFind; "main" ]
-Perform Find [ ] #
+Perform Find [ ]
+#
 #Go to record user was on before add mode.
 Sort Records [ Specified Sort Order: tagKeywordPrimary::tag; ascending ]
 [ Restore; No dialog ]
@@ -327,8 +360,6 @@ Go to Record/Request/Page
 [ First ]
 Scroll Window
 [ Home ]
-May 10, 平成27 11:19:45 Library.fp7 - addLinksPicturesToTagsMode -3-
-pictures: addLinksPicturesToTagsMode
 Loop
 Exit Loop If [ reference::_Lreference = $$returnRecord ]
 Go to Record/Request/Page
@@ -352,4 +383,4 @@ Go to Record/Request/Page [ $recordNumber ]
 #
 End If
 End If
-May 10, 平成27 11:19:45 Library.fp7 - addLinksPicturesToTagsMode -4-
+December 28, ଘ౮27 16:02:59 Library.fp7 - addLinksPicturesToTagsMode -1-
