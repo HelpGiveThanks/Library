086ee9f542926dd50bcdd838eb7cb4e7ea4e770d
David Matson <githubdavid@gmail.com>
2016-01-06 14:58:42 -0800

    Don't allow user to add both location and container tags.
    Closes #21 — Don't allow user to add both location and container tags.
    An item is either in a location or in a container.

--- scripts/tagMenu/addORremoveCitationOrReferenceRefTag.c
+++ scripts/tagMenu/addORremoveCitationOrReferenceRefTag.c
@@ -1,10 +1,21 @@
-reference(citation): addORremoveCitationOrReferenceRefTag
+tagMenu: addORremoveCitationOrReferenceRefTag
 #
 #Get current record IDs in reference or
 #observation window.
 Select Window [ Name: "References"; Current file ]
 If [ Get (LastError) = 112 ]
 Select Window [ Name: "Learn"; Current file ]
+#
+#Only allow user to have location tags for the
+#item or for the container the item is in.
+If [ TEMP::InventoryLibaryYN ≠ "" and Filter ( $$ref ; "L" ) ≠ "" ]
+Show Custom Dialog [ Message: "This item is tagged with a container tag. Remove this tag (click 'containers') before
+tagging it with its own location tags. Items can be either in a location or in a container that is in a location."; Buttons:
+“OK” ]
+Select Window [ Name: "Tag Menus"; Current file ]
+Halt Script
+End If
+#
 If [ testlearn::kcitation ≠ "" ]
 Show Custom Dialog [ Message: "References can be added after the citation is removed. (Cite the source of an unchanged
 copy, clip, quote, etc. from one source. Reference the sources of your rewording/remixing of one or more texts, movies,
@@ -34,7 +45,44 @@ citation or reference for another record. This is a known issue, and in a future
 // Exit Script [ ]
 // End If
 #
+#If the reference has media, then prepare to
+#ask user if they would like to show it.
 Set Variable [ $newRef; Value:reference::_Lreference ]
+If [ //There is a picture to show if...
+reference::picture ≠ "" or
+reference::showMedia ≠ "" and reference::URL ≠ "" or
+reference::kfileLocation ≠ "" and reference::fileName ≠ "" and Right ( reference::fileName ; 3 ) = "jpg"
+or reference::kfileLocation ≠ "" and reference::fileName ≠ "" and Right ( reference::fileName ; 3 ) = "gif"
+or reference::kfileLocation ≠ "" and reference::fileName ≠ "" and Right ( reference::fileName ; 3 ) = "eps"
+or reference::kfileLocation ≠ "" and reference::fileName ≠ "" and Right ( reference::fileName ; 3 ) = "jp2"
+or reference::kfileLocation ≠ "" and reference::fileName ≠ "" and Right ( reference::fileName ; 3 ) = "psd"
+or reference::kfileLocation ≠ "" and reference::fileName ≠ "" and Right ( reference::fileName ; 3 ) = "png"
+or reference::kfileLocation ≠ "" and reference::fileName ≠ "" and Right ( reference::fileName ; 3 ) = "pct"
+or reference::kfileLocation ≠ "" and reference::fileName ≠ "" and Right ( reference::fileName ; 3 ) = "pcs"
+or reference::kfileLocation ≠ "" and reference::fileName ≠ "" and Right ( reference::fileName ; 3 ) = ".qt"
+or reference::kfileLocation ≠ "" and reference::fileName ≠ "" and Right ( reference::fileName ; 3 ) = "sgi"
+or reference::kfileLocation ≠ "" and reference::fileName ≠ "" and Right ( reference::fileName ; 3 ) = "tga"
+or reference::kfileLocation ≠ "" and reference::fileName ≠ "" and Right ( reference::fileName ; 3 ) = "tif"
+or reference::kfileLocation ≠ "" and reference::fileName ≠ "" and Right ( reference::fileName ; 3 ) = "bmp"
+or reference::kfileLocation ≠ "" and reference::fileName ≠ "" and Right ( reference::fileName ; 3 ) = "wmf"
+or reference::kfileLocation ≠ "" and reference::fileName ≠ "" and Right ( reference::fileName ; 3 ) = "emf" or
+reference::URL ≠ "" and Right ( reference::URL ; 3 )= "jpg"
+or reference::URL ≠ "" and Right ( reference::URL ; 3 )= "gif"
+or reference::URL ≠ "" and Right ( reference::URL ; 3 )= "eps"
+or reference::URL ≠ "" and Right ( reference::URL ; 3 )= "jp2"
+or reference::URL ≠ "" and Right ( reference::URL ; 3 )= "psd"
+or reference::URL ≠ "" and Right ( reference::URL ; 3 )= "png"
+or reference::URL ≠ "" and Right ( reference::URL ; 3 )= "pct"
+or reference::URL ≠ "" and Right ( reference::URL ; 3 )= "pcs"
+or reference::URL ≠ "" and Right ( reference::URL ; 3 )= ".qt"
+or reference::URL ≠ "" and Right ( reference::URL ; 3 )= "sgi"
+or reference::URL ≠ "" and Right ( reference::URL ; 3 )= "tga"
+or reference::URL ≠ "" and Right ( reference::URL ; 3 )= "tif"
+or reference::URL ≠ "" and Right ( reference::URL ; 3 )= "bmp"
+or reference::URL ≠ "" and Right ( reference::URL ; 3 )= "wmf"
+or reference::URL ≠ "" and Right ( reference::URL ; 3 )= "emf" ]
+Set Variable [ $media; Value:1 ]
+End If
 #
 #Set record number so can return user to it when done.
 Set Variable [ $recordNumber; Value:Get (RecordNumber) ]
@@ -46,6 +94,18 @@ Select Window [ Name: "Learn"; Current file ]
 Set Field [ testlearn::kcreference; $newRef & "¶" & $$ref ]
 Set Variable [ $$ref; Value:testlearn::kcreference ]
 Go to Field [ ]
+#
+#Ask user if they would like to show the
+#reference's media if there is any to show.
+If [ $media = 1 and testlearn::Picture = "" ]
+Show Custom Dialog [ Message: "Display reference's media? Learn layouts with an unused picture area can display
+reference media. Click any reference's picture-selection button (QV layout) to toggle showing/not showing its
+media."; Buttons: “no”, “yes” ]
+If [ Get (LastMessageChoice) = 2 ]
+Set Field [ testlearn::kshowReferencedMedia; $newRef ]
+End If
+End If
+#
 Else If [ Get (LastError) ≠ 112 ]
 Set Field [ reference::lock; $newRef & "¶" & $$ref ]
 Set Variable [ $$ref; Value:reference::lock ]
@@ -86,7 +146,6 @@ End If
 #NOTE: Library used to consist of multilbe
 #sections and now it consists of only one, so
 #these next steps are no longer neccessary.
-#I'll leave them until the next version in case further testing reveals a need for them after all.
 #I'll leave them until the next version in case
 #further testing reveals a need for them after all.
 #
@@ -148,6 +207,14 @@ Substitute ( $$ref ; $removeRef & "¶" ; "" ) ;
 Substitute ( $$ref ; $removeRef; "" )
 ) ]
 Set Variable [ $$ref; Value:testlearn::kcreference ]
+#
+#Make sure the reference being removed also
+#gets its picture removed from this learn
+#record if it is being used.
+If [ $removeRef = testlearn::kshowReferencedMedia ]
+Set Field [ testlearn::kshowReferencedMedia; "" ]
+End If
+#
 Go to Field [ ]
 Else If [ Get (LastError) ≠ 112 ]
 Set Field [ reference::lock; //last item in list has no paragraph mark, so a valuecount test needs to be done and if item is not
@@ -165,7 +232,7 @@ Refresh Window
 #
 #NOTE: Further testing will reveal if these next
 #steps are necessary, but right now everything
-#works fine without them.
+#seems to work fine without them.
 // #Sort records according to users wishes.
 // If [ TEMP::sortRef = "cat" ]
 // Sort Records [ Specified Sort Order: tagKeywordPrimary::orderOrLock; ascending
@@ -184,4 +251,4 @@ Set Variable [ $$stopLoadTagRecord ]
 // Go to Record/Request/Page [ $recordNumber ]
 [ No dialog ]
 Set Variable [ $$skipFirstPartOfScript ]
-December 29, ଘ౮27 10:20:33 Library.fp7 - addORremoveCitationOrReferenceRefTag -1-
+January 6, ଘ౮28 14:56:37 Library.fp7 - addORremoveCitationOrReferenceRefTag -1-
--- scripts/tagMenu/addORremoveCiteOrRefTLTag.c
+++ scripts/tagMenu/addORremoveCiteOrRefTLTag.c
@@ -41,7 +41,8 @@ If [ testlearn::_Ltestlearn & "L¶" ≠ FilterValues ( $$ref ; testlearn::_Ltest
 #specified in learn records to be tagged as
 #being in one container at a time.
 If [ TEMP::InventoryLibaryYN ≠ "" and Filter ( $$ref ; "L" ) ≠ "" ]
-Show Custom Dialog [ Message: "This item is already tagged as being in a container (highlighted green). Untag it from this container before tagging it with a new one."; Buttons: “OK” ]
+Show Custom Dialog [ Message: "This item is already tagged as being in a container (highlighted green). Untag it from this
+container before tagging it with a new one."; Buttons: “OK” ]
 Set Variable [ $$stopLoadTagRecord ]
 Halt Script
 End If
@@ -56,14 +57,41 @@ Set Variable [ $recordNumber; Value:Get (RecordNumber) ]
 Select Window [ Name: "References"; Current file ]
 If [ Get (LastError) = 112 ]
 Select Window [ Name: "Learn"; Current file ]
+#
+#For reference libraries, add reference to list
+#of learn record's references.
+If [ TEMP::InventoryLibaryYN = "" ]
 Set Field [ testlearn::kcreference; $newRef & "¶" & $$ref ]
+#
+Else
+#For inventory libraries, only allow user to
+#have location tags for the item or for the
+#container the item is in.
+If [ $$ref ≠ "" ]
+Show Custom Dialog [ Message: "NOTE: The item in the Learn window is tagged with location tags. Items can be
+either in a location or in a container that is in a location. By placing it in a container the item's location tags will
+be replaced with the container's location tags."; Buttons: “OK”, “cancel” ]
+If [ Get ( LastMessageChoice ) = 2 ]
+Set Variable [ $$stopLoadTagRecord ]
+Halt Script
+End If
+End If
+#
+#Set new reference and delete any
+#reference-location picture.
+Set Field [ testlearn::kcreference; $newRef & "¶" ]
+Set Field [ testlearn::kshowReferencedMedia; "" ]
+End If
 Set Variable [ $$ref; Value:testlearn::kcreference ]
 Go to Field [ ]
+#
 Else If [ Get (LastError) ≠ 112 ]
+#If in the reference section, do this...
 Set Field [ reference::lock; $newRef & "¶" & $$ref ]
 Set Variable [ $$ref; Value:reference::lock ]
 Go to Field [ ]
 End If
+#
 Select Window [ Name: "Tag Menus"; Current file ]
 Refresh Window
 #
@@ -95,7 +123,8 @@ Set Variable [ $recordNumber; Value:Get (RecordNumber) ]
 Select Window [ Name: "References"; Current file ]
 If [ Get (LastError) = 112 ]
 Select Window [ Name: "Learn"; Current file ]
-Set Field [ testlearn::kcreference; //last item in list has no paragraph mark, so a valuecount test needs to be done and if item is not removed, then the removal calc without the paragraph mark is used
+Set Field [ testlearn::kcreference; //last item in list has no paragraph mark, so a valuecount test needs to be done and if item is not
+removed, then the removal calc without the paragraph mark is used
 If ( ValueCount ( $$ref) ≠ ValueCount ( Substitute ( $$ref ; $removeRef & "¶" ; "" ) ) ;
 Substitute ( $$ref ; $removeRef & "¶" ; "" ) ;
 Substitute ( $$ref ; $removeRef; "" )
@@ -103,7 +132,8 @@ Substitute ( $$ref ; $removeRef; "" )
 Set Variable [ $$ref; Value:testlearn::kcreference ]
 Go to Field [ ]
 Else If [ Get (LastError) ≠ 112 ]
-Set Field [ reference::lock; //last item in list has no paragraph mark, so a valuecount test needs to be done and if item is not removed, then the removal calc without the paragraph mark is used
+Set Field [ reference::lock; //last item in list has no paragraph mark, so a valuecount test needs to be done and if item is not
+removed, then the removal calc without the paragraph mark is used
 If ( ValueCount ( $$ref ) ≠ ValueCount ( Substitute ( $$ref ; $removeRef& "¶" ; "" ) ) ;
 Substitute ( $$ref ; $removeRef & "¶" ; "" ) ;
 Substitute ( $$ref ; $removeRef ; "" )
@@ -114,9 +144,8 @@ End If
 #
 Select Window [ Name: "Tag Menus"; Current file ]
 Refresh Window
+#
 #Sort records according to users wishes.
-July 1, 平成27 12:27:52 Library.fp7 - addORremoveCiteOrRefTLTag -1-
-tagMenu: addORremoveCiteOrRefTLTag
 If [ TEMP::InventoryLibaryYN ≠ "" ]
 Sort Records [ Specified Sort Order: testlearn::Caption; ascending ]
 [ Restore; No dialog ]
@@ -134,4 +163,4 @@ Scroll Window
 Go to Record/Request/Page [ $recordNumber ]
 [ No dialog ]
 Set Variable [ $$skipFirstPartOfScript ]
-July 1, 平成27 12:27:52 Library.fp7 - addORremoveCiteOrRefTLTag -2-
+January 6, ଘ౮28 14:55:06 Library.fp7 - addORremoveCiteOrRefTLTag -1-
